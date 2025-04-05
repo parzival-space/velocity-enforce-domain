82f2b9d5a616ab5e4fe9a83594fdb1b6cc09186a
Parzival <me@parzival.space>
2025-04-05 02:17:49 +0200

    Add allowLocalhost Option (#5)

--- src/main/java/space/parzival/minecraft/velocity/enforcedomain/EnforceDomainPlugin.java
+++ src/main/java/space/parzival/minecraft/velocity/enforcedomain/EnforceDomainPlugin.java
@@ -2,11 +2,9 @@ package space.parzival.minecraft.velocity.enforcedomain;
 
 import com.google.inject.Inject;
 import com.moandjiezana.toml.Toml;
-import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.event.ResultedEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.LoginEvent;
-import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.Player;
@@ -19,6 +17,7 @@ import net.kyori.adventure.text.Component;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -37,6 +36,7 @@ public class EnforceDomainPlugin {
     // config
     private String domain = "example.com";
     private boolean allowSubdomains = true;
+    private boolean allowLocalhost = false;
 
     @Inject
     public EnforceDomainPlugin(ProxyServer proxyServer, @DataDirectory Path dataDirectory) {
@@ -49,11 +49,10 @@ public class EnforceDomainPlugin {
     public void onProxyInitialization(ProxyInitializeEvent event) {
         // load config
         Toml config = loadConfig("config.toml");
-        String domain = config.getString("domain");
-        boolean allowSubdomains = config.getBoolean("allowSubdomains");
 
-        this.domain = domain;
-        this.allowSubdomains = allowSubdomains;
+        this.domain = config.getString("domain");
+        this.allowSubdomains = config.getBoolean("allowSubdomains");
+        this.allowLocalhost = config.getBoolean("allowLocalhost");
     }
 
     @Subscribe
@@ -61,8 +60,27 @@ public class EnforceDomainPlugin {
         Player player = event.getPlayer();
         String hostname = player.getVirtualHost().map(InetSocketAddress::getHostName).orElse(null);
 
+        // kick player if hostname is null
+        if (hostname == null) {
+            Component message = Component.empty()
+                    .content("Your client is not sending a valid hostname. Please check your client settings.")
+                    .asComponent();
+
+            player.disconnect(message);
+            event.setResult(ResultedEvent.ComponentResult.denied(message));
+
+            log.info("Player {} tried to connect without providing a hostname.", player.getUsername());
+            return;
+        }
+
         // check domain validity
-        boolean isAllowed = this.allowSubdomains ? hostname.endsWith(this.domain) : hostname.equals(this.domain);
+        boolean isLocalhost = hostname.equals(InetAddress.getLoopbackAddress().getHostName())
+                || hostname.equals("127.0.0.1")
+                || hostname.equals("::1");
+        boolean isAllowed = (isLocalhost && this.allowLocalhost)
+                || (hostname.endsWith(this.domain) && this.allowSubdomains)
+                || hostname.equals(this.domain);
+
         if (!isAllowed) {
             Component message = Component.empty()
                     .content("Direct connections to this server are not allowed.")
@@ -75,7 +93,7 @@ public class EnforceDomainPlugin {
         }
     }
 
-    public Toml loadConfig(String configFileName) {
+    private Toml loadConfig(String configFileName) {
         File configFile = new File(this.dataDirectory.toFile(), configFileName);
         if (!configFile.getParentFile().exists())
             configFile.getParentFile().mkdirs();
